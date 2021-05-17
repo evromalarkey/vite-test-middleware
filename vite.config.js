8165a0ff07bc5aa1bb16a292700b1d580b8fe79f
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2021-05-17 19:35:13 +0200

    @ fix page reload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import {resolve} from "path";
+import {resolve} from "path"
+import liveReload from 'vite-plugin-live-reload'
 
 const middleware = () => {
     return {
@@ -8,7 +9,9 @@ const middleware = () => {
             return () => {
                 viteDevServer.middlewares.use(async (req, res, next) => {
                     if (!req.originalUrl.endsWith(".html") && req.originalUrl !== "/") {
-                        req.url = req.originalUrl + ".html";
+                        req.url = `` + req.originalUrl + ".html";
+                    } else if (req.url === "/index.html") {
+                        req.url = `` + req.url;
                     }
 
                     next();
@@ -19,7 +22,10 @@ const middleware = () => {
 }
 
 export default {
-    plugins: [middleware()],
+    plugins: [middleware(), liveReload('src/templates/*.html', { alwaysReload: true, log: false, root: process.cwd() })],
+    server: {
+        open: "/"
+    },
     root: "src/templates",
     publicDir: resolve(process.cwd(), 'public'),
     resolve: {
